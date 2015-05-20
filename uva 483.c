e2a94b363d2a62bbe9beace4fb43ce2fb8fc7dde
Rabiul Awal <awalrabiul6@gmail.com>
2015-05-20 08:34:44 +0600

    Create uva 483.c

---
+++ uva 483.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main()
+{
+    char a[1000], c;
+    int i;
+    while(scanf("%s", a)!=EOF){
+            c=getchar();
+       for(i=strlen(a)-1; i>=0; i--){
+        printf("%c", a[i]);
+       }
+       putchar(c);
+    }
+    return 0;
+}
