84a5261365852b1bedbb89cff5490d262976e179
Rabiul Awal <awalrabiul6@gmail.com>
2015-05-22 10:46:32 +0600

    Create uva 10107.cpp

---
+++ uva 10107.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+    int n=1;
+    int a[10001];
+    while(scanf("%d", &a[n])==1){
+            sort(a, a+n+1);
+            if(n==1){
+                printf("%d\n", a[1]);
+            }
+            else if(n%2==0){
+                printf("%d\n", (a[n/2]+a[(n/2)+1])/2);
+            }
+            else if(n%2!=0){
+                printf("%d\n", a[(n/2)+1]);
+            }
+                n++;
+    }
+    return 0;
+}
