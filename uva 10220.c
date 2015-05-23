f47181bd98313a45583a28a4ed8493e8da0b042e
Rabiul Awal <awalrabiul6@gmail.com>
2015-05-24 03:54:01 +0600

    Update uva 10220.c

--- uva 10220.c
+++ uva 10220.c
@@ -1,36 +1,33 @@
-// UVA 10220 - I Love Big Numbers!
-// here, you can find factorial of a number which may have up to 1000 digits.
-// and then we add all the digits as question requires and print that.
-// Author: Rabiul  Awal, NSTU_CSTE
-
 #include <stdio.h>
+
+int a[10000];
+int m, n, x, temp, sum, i, j;
 
 int main()
 {
-    int a[1000];
-    int m, n, x, temp, sum, i, j;
+
     while(scanf("%d", &n)==1){
-        a[0]=1; //initializes array with only 1 digit, the digit 1.
-        m=1; // initializes digit counter
+        a[0]=1;
+        m=1;
         sum=0;
-        temp=0; //Initializes carry variable to 0.
+        temp=0;
         for(i=1; i<=n; i++){
             for(j=0; j<m; j++){
-                x=a[j]*i+temp;//x contains the digit by digit product
-                a[j]=x%10; //Contains the digit to store in position j
-                temp=x/10; //Contains the carry value that will be stored on later indexes
+                x=a[j]*i+temp;
+                a[j]=x%10;
+                temp=x/10;
             }
-            while(temp>0){ //while loop that will store the carry value on array.
+            while(temp>0){
                 a[m]=temp%10;
                 temp=temp/10;
-                m++; // increments digit counter
+                m++;
             }
         }
+        a[m]='\0';
         for(j=m-1; j>=0; j--){
             sum+=a[j];
         }
-        printf("%d", sum); //here is sum of all digits.
-        printf("\n");
+        printf("%d\n", sum);
     }
     return 0;
 }
