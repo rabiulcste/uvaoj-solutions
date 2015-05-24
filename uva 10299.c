eaafcc42b0ee91e10ebe9c94d01d40379e7ce650
Rabiul Awal <awalrabiul6@gmail.com>
2015-05-24 18:34:33 +0600

    Create uva 10299.c

---
+++ uva 10299.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int totient(int n)
+{
+    int i, ans = n;
+    if(n==1)
+        return 0;
+    for (i=2; i*i<=n; i++)
+    {
+        if (n%i==0)
+            ans-=ans/i;
+
+        while(n%i==0)
+            n/=i;
+    }
+
+    if (n!=1)
+        ans-=ans/n;
+    return ans;
+}
+
+int main()
+{
+    int n;
+        while(scanf("%d",&n) && n!=0){
+        printf("%d\n", totient(n));
+    }
+    return 0;
+}
+
