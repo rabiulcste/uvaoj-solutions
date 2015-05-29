7a1cb6331570c444081e2c1b0f0e513fb77b31ab
Rabiul Awal <awalrabiul6@gmail.com>
2015-05-30 02:08:11 +0600

    Update uva 476.c

--- uva 476.c
+++ uva 476.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
-#include <string.h>
 
 int main()
 {
-    int x, y, i, j, count;
-    char a[100], b[100];
-    while(scanf("%s %s", a, b)!=EOF){
-        x = strlen(a);
-        y = strlen(b);
-        count = 0;
-        for(i=0; i<y; i++){
-            for(j=0; j<x; j++){
-                if(b[i]==a[j]){
-                    count++;
-                }
+    int i, j, count, flag;
+    double ax[12], ay[12], cx[12], cy[12], x, y;
+    char a[20];
+    i=0;
+
+    while(scanf("%s", a) && strcmp(a, "*")!=0){
+        scanf("%lf %lf %lf %lf", &ax[i], &ay[i], &cx[i], &cy[i]);
+        i++;
+    }
+        count=0;
+    while(scanf("%lf %lf", &x, &y) && !(x==9999.9 && y==9999.9)){
+            count++;
+             flag = 0;
+        for(j=0; j<i; j++){
+        if(ax[j]<x && x<cx[j] && ay[j]>y && y>cy[j]){
+            printf("Point %d is contained in figure %d\n",  count, j+1);
+                flag = 1;
             }
         }
-        if(count>=y)
-            printf("Yes\n");
-        else
-            printf("No\n");
+        if(flag==0)
+            printf("Point %d is not contained in any figure\n",  count);
     }
     return 0;
 }
