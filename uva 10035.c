5a3bf2d8ca421d316ffe0abdb9769f997c1ca471
Rabiul Awal <awalrabiul6@gmail.com>
2015-06-01 14:54:47 +0600

    Update uva 10035.c

--- uva 10035.c
+++ uva 10035.c
@@ -1,32 +1,26 @@
 #include <stdio.h>
-#include <string.h>
 
 int main()
 {
-    int x, y, count, res;
-    char a[20], b[20];
-    while(scanf("%s %s", a, b) && strcmp(a, "0")!=0 && strcmp(b, "0")!=0){
-        x = strlen(a);
-        y = strlen(b);
-        a[x] = '\0';
-        b[y] = '\0';
-        count = 0;
-        while(x>=0 && y>=0){
-            res=a[x-1]+b[y-1]-48-48;
-            if(res>=10){
-                count++;
-            }
-            x--;
-            y--;
-                if(x<0 || y<0)
-                break;
+    long long int a, b;
+    int count, res;
+    while(scanf("%llu %llu", &a, &b)==2){
+        if(a==0 && b==0)
+            break;
+        res=0;
+        count=0;
+        while(a||b){
+            res=((a%10)+(b%10)+res)/10;
+            a/=10;
+            b/=10;
+            count+=res;
         }
         if(count==0)
             printf("No carry operation.\n");
         else if(count==1)
-            printf("%d carry opeartion.\n", count);
+            printf("1 carry operation.\n");
         else
-            printf("%d carry opeartions.\n", count);
+            printf("%d carry operations.\n", count);
     }
     return 0;
 }
