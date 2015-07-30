31477db1ff6edff2638a266826f1045eea215ca9
Rabiul Awal <awalrabiul6@gmail.com>
2015-07-31 01:32:29 +0600

    Create uva 900.c

---
+++ uva 900.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int fib[100];
+    int n, i;
+    while(scanf("%d", &n) && n>0){
+        fib[0]=1;
+        fib[1]=1;
+        for(i=2; i<=n; i++){
+            fib[i]=fib[i-1]+fib[i-2];
+        }
+        printf("%d\n", fib[n]);
+    }
+    return 0;
+}
