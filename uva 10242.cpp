f0413b1cc0d1fbc8d37095921f63d58edcf400ae
Rabiul Awal <awalrabiul6@gmail.com>
2015-08-10 03:15:58 +0600

    Create uva 10242.cpp

---
+++ uva 10242.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    double x1, y1, x2, y2, x3, y3, x4, y4, x, y;
+    while(scanf("%lf %lf %lf %lf %lf %lf %lf %lf", &x1, &y1, &x2, &y2, &x3, &y3, &x4, &y4)==8) {
+
+        if(x1==x3 && y1==y3){
+            x = x2-x1+x4;
+            y = y2-y1+y4;
+        }
+        else if(x1==x4 && y1==y4){
+            x = x2-x1+x3;
+            y = y2-y1+y3;
+        }
+        else if(x2==x3 && y2==y3){
+            x = x1-x2+x4;
+            y = y1-y2+y4;
+        }
+
+        else if(x2==x4 && y2==y4){
+            x = x1-x2+x3;
+            y = y1-y2+y3;
+        }
+        printf("%.3lf %.3lf\n", x, y);
+    }
+    return 0;
+}
+
