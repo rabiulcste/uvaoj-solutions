b03fb054c40888ecf83e862e4ba2e94fb76ca7ea
Rabiul Awal <awalrabiul6@gmail.com>
2015-09-29 18:54:01 +0600

    Create uva 12751.cpp

---
+++ uva 12751.cpp
@@ -0,0 +1,21 @@
+#include <bits//stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t, n, k, x, kase = 0;
+    scanf("%d", &t);
+    while(t--)
+    {
+        scanf("%d %d %d", &n, &k, &x);
+        int cal = n*(n+1)/2;
+        int ans = 0;
+        while(k--)
+        {
+            ans+=x;
+            x++;
+        }
+        printf("Case %d: %d\n", ++kase, cal-ans);
+    }
+}
