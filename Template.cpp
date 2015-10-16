b374ed64c300cf8a788e7c95fe2c6a65b95588b4
Rabiul Awal <awalrabiul6@gmail.com>
2015-10-17 02:46:23 +0600

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <set>
+#include <iomanip>
+#include <list>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <numeric>
+
+#include <cstdio>
+#include <cmath>
+#include <climits>
+#include <cstring>
+#include <cctype>
+#include <cstdlib>
+
+#include <stdio.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef set<int> si;
+
+int main(int argc, char* argv[]) {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+#endif
+
+	int T;
+	cin>>T;
+	while (T--) {
+		
+	}
+	return 0;
+}
