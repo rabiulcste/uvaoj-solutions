bcde211f36ecb8143e557d6737de81f7a0a72145
Rabiul Awal <awalrabiul6@gmail.com>
2015-12-01 15:55:35 +0600

    Contest Basic

---
+++ A Sample.cpp
@@ -0,0 +1,19 @@
+#include <bits//stdc++.h>
+
+using namespace std;
+
+#define PI 3.1416
+#define MX 1000000
+#define INF 99999999
+#define pb push_back
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef map<string, int> mp;
+
+
+int main()
+{
+    cout<<"Hello"<<endl;
+}
