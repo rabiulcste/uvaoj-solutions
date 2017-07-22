e59a46f61b689a49edb1f5c6df1572aad4ea55f1
Rabiul Awal <awalrabiul6@gmail.com>
2017-07-23 03:16:25 +0600

    adhoc

--- uva 10055.c
+++ uva 10055.c
@@ -3,12 +3,13 @@
 int main()
 {
     long long a, b, result;
-    while(scanf("%lld %lld", &a, &b)==2){
-        if(a>b)
-        result=a-b;
-    else
-        result=b-a;
+    while(scanf("%lld %lld", &a, &b) == 2){
+        if(a > b)
+            result = a - b;
+        else
+            result = b - a;
         printf("%lld\n", result);
     }
+    
     return 0;
 }
