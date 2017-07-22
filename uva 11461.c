8a08e57352ecfe68f3002ceed2bf8ac2431707a6
Rabiul Awal <awalrabiul6@gmail.com>
2017-07-23 03:10:20 +0600

    adhoc

--- uva 11461.c
+++ uva 11461.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <math.h>
+
 int main()
 {
     long long a, b, i, count;
+    
     while(scanf("%lld %lld", &a, &b) && a>0 && b>0){
             count=0;
         for(i=a; i<=b; i++){
@@ -12,6 +14,5 @@ int main()
         }
     printf("%lld\n", count);
     }
-    
     return 0;
 }
